45e81807113753d61cf17fc7be950eea0af05c31
Litaaya <91871662+Litaaya@users.noreply.github.com>
2023-05-27 15:05:54 +0700

    Update KeyHandler.java

--- FlappyBird/handlers/KeyHandler.java
+++ FlappyBird/handlers/KeyHandler.java
@@ -2,10 +2,18 @@ package handlers;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
 import main.Game;
+import main.Sound;
 
 public class KeyHandler implements KeyListener {
+
+    private Sound sound;
+
+    public KeyHandler(Sound sound) {
+        this.sound = sound;
+        sound.setFile(0); // Load the sound file
+    }
+    
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -16,6 +24,7 @@ public class KeyHandler implements KeyListener {
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_SPACE) {
             Game.bird.setVelY(-6);
+            sound.play(); 
         }
     }
 
